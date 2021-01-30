22a4c38c098224a680e68d9d7bcbce3958a7c30b
sgeletta <slg@kdsol.com>
2021-01-30 23:45:33 +0000

    my changes

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+  p = document.getElementById("principal").value;
+  r = document.getElementById("myRange").value;
+  y = document.getElementById("years").value;
+  result = document.getElementById("myRange").value;
+result.innerHTML = "The interest is " + (p*n*r/100);
+};
     
+function sliderChange(val) {
+document.getElementById('myRange').innerHTML = val;
 }
         

a6288372e5dc71d16284f27ca8ca81142d58e4c0
sgeletta <slg@kdsol.com>
2021-01-31 14:36:03 -0600

    some more additional feature edits

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function compute()
 {
   p = document.getElementById("principal").value;
  document.getElementByID("reslt").innerHTML = p; 
-};
+}
     
 function slc() {
  var x = document.getElementById("myRange").value;

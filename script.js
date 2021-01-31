2ef5eea6f0bde4119ac88e033040ea47e7b887e3
sgeletta <slg@kdsol.com>
2021-01-31 13:15:01 -0600

    some more additional feature edits

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function compute()
   y = document.getElementById("years").value;
   yrs = 2021 + y;
   interest = p*n*r/100;
-  document.getElementByID("result").innerHTML = "If you deposit " + p + ", at an interest rate of " + r + "% you will receive an amount of " + interest + ", in the year " + yrs; 
+
+  document.getElementByID("reslt").innerHTML = "If you deposit " + p + ", at an interest rate of " + r + "% you will receive an amount of " + interest + ", in the year " + yrs; 
 };
     
 function slc() {

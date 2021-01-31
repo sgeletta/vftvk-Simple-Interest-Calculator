7a19a0a40b806b95da001bfa4a4908fa002b6b06
sgeletta <slg@kdsol.com>
2021-01-31 11:27:16 -0600

    some more fixes

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ function compute()
   p = document.getElementById("principal").value;
   r = document.getElementById("myRange").value;
   y = document.getElementById("years").value;
-  result = document.getElementById("myRange").value;
-result.innerHTML = "The interest is " + (p*n*r/100);
+  interest = document.getElementByID("result").innerHTML = (p*n*r/100);
 };
     
-function sliderChange(val) {
+function slc() {
  var x = document.getElementById("myRange").value;
 	document.getElementById("res").innerHTML = x;
 }

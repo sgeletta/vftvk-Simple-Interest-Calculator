d5e698d98c2c3ef14b97b347f956a584254f072a
sgeletta <slg@kdsol.com>
2021-02-01 22:24:12 +0000

    final validation

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function compute()
 {
-	var p = document.getElementById("principal").value;
+  
+  	var p = document.getElementById("principal").value;
 	var r = document.getElementById("myRange").value;
 	var y = document.getElementById("years").value;
 	var yrs = 2021 + parseInt(y);
-	var i = p*r*y/100;
+    var i = p*r*y/100;
+    if (p < 1){
+     window.alert("input to principal needs to be positive");
+    }
+    else
 	document.getElementById("reslt").innerHTML = "If you deposit " + p + ", at an interest rate of " + r + "% you will receive an amount of " + i + ", in the year " + yrs; 
 }
     
